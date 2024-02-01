329573d78ec5c97b7bcffe3e96b4f9dff4a30369
Miroslav Balaz <gpslayer@gmail.com>
2024-02-01 11:02:14 +0100

    Update compaction_test.go
    added AddSameKeyTooMuch

--- test/compaction_test.go
+++ test/compaction_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	. "github.com/onsi/gomega"
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -41,6 +42,18 @@ func TestCompaction(t *testing.T) {
 		deleteEntries(ctx, g, client, 1, 2)
 	})
 
+	t.Run("AddSameKeyTooMuch", func(t *testing.T) {
+		g := NewWithT(t)
+
+		// Add a large number of entries
+		numAddEntries := 4000
+		timedCtx, cancel := context.WithTimeout(ctx, time.Second*30)
+		defer cancel()
+		addSameEntries(timedCtx, g, client, numAddEntries)
+		//TODO: cleanup?
+
+	})
+
 	t.Run("LargeDatabaseDeleteFivePercent", func(t *testing.T) {
 		g := NewWithT(t)
 
@@ -74,7 +87,23 @@ func addEntries(ctx context.Context, g Gomega, client *clientv3.Client, numEntri
 		addEntry(ctx, g, client, key, value)
 	}
 }
+func addSameEntries(ctx context.Context, g Gomega, client *clientv3.Client, numEntries int) {
+	for i := 0; i < numEntries; i++ {
+		// TODO: this is to show progress and gradual decay in performance
+		if i%100 == 0 {
+			print(i)
+		}
+		key := "testkey-same"
+		value := fmt.Sprintf("value-%d", i)
+		if i != 0 {
+			updateEntry(ctx, g, client, key, value)
 
+		} else {
+			addEntry(ctx, g, client, key, value)
+		}
+	}
+	//TODO: check if there are in fact co many entries, but it is seen from gradual decay in performance
+}
 func addEntry(ctx context.Context, g Gomega, client *clientv3.Client, key string, value string) {
 	resp, err := client.Txn(ctx).
 		If(clientv3.Compare(clientv3.ModRevision(key), "=", 0)).
@@ -83,6 +112,22 @@ func addEntry(ctx context.Context, g Gomega, client *clientv3.Client, key string
 
 	g.Expect(err).To(BeNil())
 	g.Expect(resp.Succeeded).To(BeTrue())
+}
+func updateEntry(ctx context.Context, g Gomega, client *clientv3.Client, key string, value string) {
+	resp, err := client.Get(ctx, key, clientv3.WithRange(""))
+
+	g.Expect(err).To(BeNil())
+	g.Expect(resp.Kvs).To(HaveLen(1))
+
+	resp2, err2 := client.Txn(ctx).
+		If(clientv3.Compare(clientv3.ModRevision(key), "=", resp.Kvs[0].ModRevision)).
+		Then(clientv3.OpPut(key, value)).
+		Else(clientv3.OpGet(key, clientv3.WithRange(""))).
+		Commit()
+
+	g.Expect(err2).To(BeNil())
+	g.Expect(resp2.Succeeded).To(BeTrue())
+
 }
 
 func deleteEntries(ctx context.Context, g Gomega, client *clientv3.Client, start int, end int) {
